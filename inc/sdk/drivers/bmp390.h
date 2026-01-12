688d69072081052c844430a5562f32dcae3703a1
rwilliaise <12103838+rwilliaise@users.noreply.github.com>
2026-01-12 15:20:52 -0800

    Block out the BMP390 driver

---
+++ inc/sdk/drivers/bmp390.h
@@ -0,0 +1,16 @@
+
+#ifndef AIRBRAKES_SDK_BMP390_H_
+#define AIRBRAKES_SDK_BMP390_H_
+
+namespace sdk {
+
+class bmp390 {
+public:
+
+
+};
+
+} // namespace sdk
+
+
+#endif // AIRBRAKES_SDK_BMP390_H_
---
+++ src/drivers/bmp390.cc
@@ -0,0 +1,4 @@
+
+#include <sdk/drivers/bmp390.h>
+
+
