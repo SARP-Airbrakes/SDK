af4cc500d38f203355f01e827894664379299bc5
rwilliaise <12103838+rwilliaise@users.noreply.github.com>
2026-01-17 18:24:27 -0800

    Finish BMI088 driver

--- inc/sdk/drivers/bmi088.h
+++ inc/sdk/drivers/bmi088.h
@@ -16,12 +16,37 @@ public:
     static constexpr int SLAVE_ADDRESS_ACC = 0x18;
     static constexpr int SLAVE_ADDRESS_GYRO = 0x68;
 
-    static constexpr int ACC_CHIP_ID = 0x1E;
+    static constexpr int ACC_CHIP_ID = 0x1e;
+
     static constexpr int ACC_CHIP_ID_ADDR = 0x00;
     static constexpr int ACC_X_LSB_ADDR = 0x12;
+    static constexpr int ACC_CONF_ADDR = 0x40;
+    static constexpr int ACC_RANGE_ADDR = 0x41;
 
-    static constexpr float GRAVITY_EARTH = 9.80665f;
+    static constexpr int RATE_X_LSB_ADDR = 0x02;
+    static constexpr int GYRO_RANGE_ADDR = 0x0f;
+    static constexpr int GYRO_BANDWIDTH_ADDR = 0x10;
 
+    /** Accelerometer low-pass filter bandwidth. see 4.4.1 and 5.3.8 */
+    enum class acc_bwp : uint8_t {
+        OSR4 = 0x00, /* 4-fold oversampling */
+        OSR2 = 0x01, /* 2-fold oversamping */
+        NORMAL = 0x02, /* no oversampling */
+    };
+
+    /** Accelerometer output data rate. See 5.3.8 */
+    enum class acc_odr : uint8_t {
+        ODR_12_5HZ = 0x05, /* 12.5 Hz */
+        ODR_25HZ = 0x06, /* 25 Hz */
+        ODR_50HZ = 0x07, /* 50 Hz */
+        ODR_100HZ = 0x08, /* 100 Hz */
+        ODR_200HZ = 0x09, /* 200 Hz */
+        ODR_400HZ = 0x0a, /* 400 Hz */
+        ODR_800HZ = 0x0b, /* 800 Hz */
+        ODR_1600HZ = 0x0c, /* 1600 Hz */
+    };
+
+    /** Accelerometer range setting. See 5.3.9 */
     enum class acc_range : uint8_t {
         RANGE_3G = 0x00,
         RANGE_6G = 0x01,
@@ -29,16 +54,52 @@ public:
         RANGE_24G = 0x03,
     };
 
-public:
+    /** Gyroscope range setting. See 5.5.4 */
+    enum class gyro_range : uint8_t {
+        RANGE_2000DPS = 0x00, /* 2000 deg/s */
+        RANGE_1000DPS = 0x01, /* 1000 deg/s */
+        RANGE_500DPS = 0x02, /* 500 deg/s */
+        RANGE_250DPS = 0x03, /* 250 deg/s */
+        RANGE_125DPS = 0x04, /* 125 deg/s */
+    };
+
+    /** Gyroscope output data rate and filter bandwidth setting. See 5.5.5  */
+    enum class gyro_bw : uint8_t {
+        BW_532HZ = 0x00, /* BW: 532Hz, ODR: 2000Hz */
+        BW_230HZ = 0x01, /* BW: 230Hz, ODR: 2000Hz */
+        BW_116HZ = 0x02, /* BW: 116Hz, ODR: 1000Hz */
+        BW_47HZ = 0x03, /* BW: 47Hz, ODR: 400Hz */
+        BW_23HZ = 0x04, /* BW: 23Hz, ODR: 200Hz */
+        BW_12HZ = 0x05, /* BW: 12Hz, ODR: 100Hz */
+        BW_64HZ = 0x06, /* BW: 64Hz, ODR: 200Hz */
+        BW_32HZ = 0x07, /* BW: 32Hz, ODR: 100Hz */
+    };
+
     using real = float;
+
+    /* seconds per sensortime lsb */
+    static constexpr real SENSORTIME_RESOLUTION = 1.0f / 256000.0f;
+    static constexpr real GRAVITY_EARTH = 9.80665f; /* m/s^2 */
 
     struct vec3 {
         real x, y, z;
     };
     
     struct state {
-        vec3 acceleration_ms2;
-        vec3 orientation_deg;
+        vec3 acceleration_ms2; /* in m/s^2 */
+        vec3 orientation_deg; /* in deg */
+        vec3 angular_velocity_ds; /* in deg/s */
+
+        uint32_t last_sensortime;
+        uint32_t sensortime;
+        bool uninitialized_sensortime = true;
+
+        acc_range acc_range = acc_range::RANGE_6G;
+        acc_bwp acc_bwp = acc_bwp::NORMAL;
+        acc_odr acc_odr = acc_odr::ODR_100HZ;
+
+        gyro_range gyro_range = gyro_range::RANGE_2000DPS;
+        gyro_bw gyro_bw = gyro_bw::BW_532HZ;
     };
 
 public:
@@ -46,6 +107,16 @@ public:
     bmi088(sdk::i2c_master &i2c) : i2c(i2c)
     {
     }
+
+    /** Start the accelerometer and gyroscope. Thread-safe blocking. */
+    void start();
+    /** Stop the accelerometer and gyroscope. Thread-safe blocking. */
+    void stop();
+
+    /** Sets the configuration of the accelerometer. Thread-safe blocking. */
+    void set_acc_config(acc_range range, acc_bwp bwp, acc_odr odr);
+    /** Sets the configuration of the gyroscope. Thread-safe blocking. */
+    void set_gyro_config(gyro_range range, gyro_bw bw);
 
     /** Gets if this chip is connected. Thread-safe blocking. */
     bool is_connected();
@@ -62,11 +133,17 @@ public:
     state copy_state();
 
 private:
+    real sensortime_to_s(uint32_t sensortime);
+
+    /** Gets the difference (in s) between two sensortimes. */
+    real get_delta_t(uint32_t last_sensortime, uint32_t sensortime);
+
     bool fetch_acc_data(state &out);
+    bool fetch_gyro_data(state &out);
     bool fetch_data(state &out);
 
     sdk::i2c_master &i2c;
-    acc_range curr_range;
+
     state internal_state;
     mutex state_mutex;   
 };
--- src/drivers/bmi088.cc
+++ src/drivers/bmi088.cc
@@ -5,6 +5,102 @@
 
 namespace sdk {
 
+void bmi088::set_acc_config(acc_range range, acc_bwp bwp, acc_odr odr)
+{
+    acc_range curr_range;
+    acc_bwp curr_bwp;
+    acc_odr curr_odr;
+
+    // scoped read from internal state
+    {
+        scoped_lock lock(state_mutex);
+        curr_range = internal_state.acc_range;
+        curr_bwp = internal_state.acc_bwp;
+        curr_odr = internal_state.acc_odr;
+    }
+
+    if (range != curr_range) { 
+        auto status = i2c.write(
+            SLAVE_ADDRESS_ACC << 1,
+            ACC_RANGE_ADDR,
+            (uint8_t *) &range,
+            1,
+            false
+        );
+        if (status != i2c_master::status::OK) {
+            /* TODO: error condition */
+            return;
+        }
+        scoped_lock lock(state_mutex);
+        internal_state.acc_range = range;
+    }
+
+    if (bwp != curr_bwp || odr != curr_odr) {
+        uint8_t acc_conf = 0;
+        acc_conf |= (uint8_t)odr;
+        acc_conf |= ((uint8_t)bwp) << 4;
+        acc_conf |= 1 << 7; /* last bit must always be 1 (see 5.3.8) */
+        auto status = i2c.write(
+            SLAVE_ADDRESS_ACC << 1,
+            ACC_CONF_ADDR,
+            &acc_conf,
+            1,
+            false
+        );
+        if (status != i2c_master::status::OK) {
+            /* TODO: error condition */
+            return;
+        }
+        scoped_lock lock(state_mutex);
+        internal_state.acc_bwp = bwp;
+        internal_state.acc_odr = odr;
+    }
+}
+
+void bmi088::set_gyro_config(gyro_range range, gyro_bw bw)
+{
+    gyro_range curr_range;
+    gyro_bw curr_bw;
+
+    {
+        scoped_lock lock(state_mutex);
+        curr_range = internal_state.gyro_range;
+        curr_bw = internal_state.gyro_bw;
+    }
+    
+    if (range != curr_range) {
+        auto status = i2c.write(
+            SLAVE_ADDRESS_GYRO << 1,
+            GYRO_RANGE_ADDR,
+            (uint8_t *) &range,
+            1,
+            false
+        );
+        if (status != i2c_master::status::OK) {
+            /* TODO: error condition */
+            return;
+        }
+        scoped_lock lock(state_mutex);
+        internal_state.gyro_range = range;
+    }
+
+    if (bw != curr_bw) {
+        auto status = i2c.write(
+            SLAVE_ADDRESS_GYRO << 1,
+            GYRO_BANDWIDTH_ADDR,
+            (uint8_t *) &bw,
+            1,
+            false
+        );
+        if (status != i2c_master::status::OK) {
+            /* TODO: error condition */
+            return;
+        }
+        scoped_lock lock(state_mutex);
+        internal_state.gyro_bw = bw;
+    }
+}
+
 bool bmi088::is_connected()
 {
     // just check for the acc
@@ -25,10 +121,20 @@ bool bmi088::is_connected()
 void bmi088::update()
 {
     state out;
+
+    // first, copy the state (for stuff like last_sensortime, etc)
+    {
+        scoped_lock lock(state_mutex);
+        out = internal_state;
+    }
+
+    // fetch relevant data
     if (!fetch_data(out)) {
+        /* TODO: error condition */
         return;
     }
     
+    // then copy it back
     scoped_lock lock(state_mutex);
     internal_state = out;
 }
@@ -40,23 +146,42 @@ bmi088::state bmi088::copy_state()
     return internal_state;
 }
 
-static uint8_t get_range_multiplier(bmi088::acc_range range)
+bmi088::real bmi088::sensortime_to_s(uint32_t sensortime)
+{
+    return SENSORTIME_RESOLUTION * (real)sensortime;
+}
+
+bmi088::real bmi088::get_delta_t(uint32_t last_sensortime, uint32_t sensortime)
+{
+    // overflow has occurred
+    if (sensortime < last_sensortime)
+        sensortime += 0x00ffffff;
+    real out = sensortime_to_s(sensortime) - sensortime_to_s(last_sensortime);
+
+    // really make sure that there is no negative times
+    if (out < 0)
+        return 0;
+    return out;
+}
+
+static bmi088::real get_acc_range_multiplier(bmi088::acc_range range)
 {
     switch (range) {
     case bmi088::acc_range::RANGE_3G:
-        return 3;
+        return 3.0f;
     case bmi088::acc_range::RANGE_6G:
-        return 6;
+        return 6.0f;
     case bmi088::acc_range::RANGE_12G:
-        return 12;
+        return 12.0f;
     case bmi088::acc_range::RANGE_24G:
-        return 24;
+        return 24.0f;
     }
 }
 
+
 bool bmi088::fetch_acc_data(state &out)
 {
-    uint8_t data_frame[6];
+    uint8_t data_frame[9];
     auto status = i2c.read(
         SLAVE_ADDRESS_ACC << 1,
         ACC_X_LSB_ADDR,
@@ -65,24 +190,79 @@ bool bmi088::fetch_acc_data(state &out)
         false
     );
     if (status != i2c_master::status::OK) {
+        /* TODO: error condition */
         return false;
     }
     int16_t accel_x = (data_frame[1] << 8) | data_frame[0];
     int16_t accel_y = (data_frame[3] << 8) | data_frame[2];
     int16_t accel_z = (data_frame[5] << 8) | data_frame[4];
+    uint32_t sensortime = (data_frame[8] << 16) | (data_frame[7] << 8) |
+        data_frame[6];
 
-    uint8_t range = get_range_multiplier(curr_range);
+    real mult = get_acc_range_multiplier(out.acc_range);
     
     /* see 5.3.4 */
-    out.acceleration_ms2.x = (GRAVITY_EARTH * (real)accel_x * range) / 32768.0f;
-    out.acceleration_ms2.y = (GRAVITY_EARTH * (real)accel_y * range) / 32768.0f;
-    out.acceleration_ms2.z = (GRAVITY_EARTH * (real)accel_z * range) / 32768.0f;
+    out.acceleration_ms2.x = (GRAVITY_EARTH * (real)accel_x * mult) / 32768.0f;
+    out.acceleration_ms2.y = (GRAVITY_EARTH * (real)accel_y * mult) / 32768.0f;
+    out.acceleration_ms2.z = (GRAVITY_EARTH * (real)accel_z * mult) / 32768.0f;
+
+    // its better to have a delta-T of 0 rather than a large amount
+    out.last_sensortime = out.uninitialized_sensortime ? sensortime :
+        out.sensortime;
+    out.uninitialized_sensortime = false;
+    out.sensortime = sensortime;
+    return true;
+}
+
+static bmi088::real get_gyro_range_multiplier(bmi088::gyro_range range)
+{
+    switch (range) {
+    case bmi088::gyro_range::RANGE_2000DPS:
+        return 2000.0f;
+    case bmi088::gyro_range::RANGE_1000DPS:
+        return 1000.0f;
+    case bmi088::gyro_range::RANGE_500DPS:
+        return 500.0f;
+    case bmi088::gyro_range::RANGE_250DPS:
+        return 250.0f;
+    case bmi088::gyro_range::RANGE_125DPS:
+        return 125.0f;
+    }
+}
+
+bool bmi088::fetch_gyro_data(state &out)
+{
+    uint8_t data_frame[6];
+    auto status = i2c.read(
+        SLAVE_ADDRESS_GYRO << 1,
+        RATE_X_LSB_ADDR,
+        data_frame,
+        sizeof(data_frame),
+        false
+    );
+    if (status != i2c_master::status::OK) {
+        /* TODO: error condition */
+        return false;
+    }
+    int16_t rate_x = (data_frame[1] << 8) | data_frame[0];
+    int16_t rate_y = (data_frame[3] << 8) | data_frame[2];
+    int16_t rate_z = (data_frame[5] << 8) | data_frame[4];
+
+    real mult = get_gyro_range_multiplier(out.gyro_range);
+    out.angular_velocity_ds.x = (rate_x * mult) / 32768.0f;
+    out.angular_velocity_ds.y = (rate_y * mult) / 32768.0f;
+    out.angular_velocity_ds.z = (rate_z * mult) / 32768.0f;
+
+    real delta_t = get_delta_t(out.last_sensortime, out.sensortime);
+    out.orientation_deg.x += out.angular_velocity_ds.x * delta_t;
+    out.orientation_deg.y += out.angular_velocity_ds.y * delta_t;
+    out.orientation_deg.z += out.angular_velocity_ds.z * delta_t;
     return true;
 }
 
 bool bmi088::fetch_data(state &out)
 {
-    return fetch_acc_data(out);
+    return fetch_acc_data(out) && fetch_gyro_data(out);
 }
 
 } // namespace sdk
