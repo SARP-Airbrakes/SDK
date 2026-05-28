c509c6e93ecbfd23b3362f95743e8296994c3266
rwilliaise <12103838+rwilliaise@users.noreply.github.com>
2026-05-27 19:21:14 -0700

    Calibration sequence added.

--- inc/sdk/drivers/motor_controller.h
+++ inc/sdk/drivers/motor_controller.h
@@ -6,6 +6,7 @@
 #include <sdk/drivers/quad_encoder.h>
 
 #include <utility>
+#include <optional>
 
 namespace sdk {
 
@@ -15,15 +16,29 @@ namespace sdk {
  */
 class motor_controller {
 public:
+
+    /* amount of degrees per second that constitutes a stall */
+    static constexpr float MIN_STALL_DPS = 1;
+    /* time in seconds where the motor has to stall for calibration to finish */
+    static constexpr float MIN_CALIBRATION_STALL_DELTA = 0.2;
+
+    enum class state {
+        OFF,
+        ACTIVE,
+        CALIBRATING
+    };
+    
+public:
     motor_controller(
-        float p, float i, float d,
+        float p, float i, float d, int calibration_encoder_offset,
         drv8701 &&motor,
         quad_encoder &&encoder
     ) : p(p), i(i), d(d), target_motor(std::move(motor)),
-            encoder(std::move(encoder))
+            encoder(std::move(encoder)), calibration_encoder_offset(calibration_encoder_offset)
     {
     }
 
+    void calibrate();
     void start();
     void stop();
 
@@ -40,9 +55,22 @@ public:
     float integral_error = 0;
     float last_error = 0;
 
+    std::optional<float> last_encoder_degrees = 0;
+    std::optional<float> calibration_hold_time = 0;
+
+private:
+    void switch_states(state new_state);
+
+    void update_pid(float dt);
+    void update_calibration(float dt);
+
 public:
     drv8701 target_motor;
     quad_encoder encoder;
+
+    int calibration_encoder_offset;
+
+    state current_state;
     
 };
 
--- src/drivers/motor_controller.cc
+++ src/drivers/motor_controller.cc
@@ -1,15 +1,25 @@
 
 #include <sdk/drivers/motor_controller.h>
 
+#include <sdk/timing.h>
+
 namespace sdk {
+
+void motor_controller::calibrate()
+{
+    if (current_state == state::ACTIVE)
+        switch_states(state::CALIBRATING);
+}
 
 void motor_controller::start()
 {
+    switch_states(state::ACTIVE);
     target_motor.start();
 }
 
 void motor_controller::stop()
 {
+    switch_states(state::OFF);
     target_motor.stop();
 }
 
@@ -20,10 +30,41 @@ void motor_controller::set_target_degrees(float new_target)
 
 void motor_controller::update_motor(float dt)
 {
+    switch (current_state) {
+    case state::ACTIVE:
+        update_pid(dt);
+        break;
+    case state::CALIBRATING:
+        update_calibration(dt);
+        break;
+    default:
+        break;
+    }
+}
+
+void motor_controller::switch_states(state new_state)
+{
+    switch (current_state) {
+    case state::ACTIVE:
+        last_error = 0;
+        integral_error = 0;
+        break;
+    case state::CALIBRATING:
+        last_encoder_degrees = std::nullopt;
+        calibration_hold_time = std::nullopt;
+        break;
+    default:
+        break;
+    }
+    current_state = new_state;
+}
+
+void motor_controller::update_pid(float dt)
+{
     float curr_degrees = encoder.get_degrees();
     float error = target_degrees - curr_degrees;
 
-    integral_error = error * dt;
+    integral_error += error * dt;
 
     // reset integral error if error switches sign
     if ((last_error <= 0) != (error <= 0)) {
@@ -44,6 +85,33 @@ void motor_controller::update_motor(float dt)
     commanded_power = output;
 
     target_motor.set_power(output);
+}
+
+void motor_controller::update_calibration(float dt)
+{
+    target_motor.set_power(-0.6f);
+    if (!last_encoder_degrees) {
+        last_encoder_degrees = encoder.get_degrees();
+    } else {
+        float current_encoder_degrees = encoder.get_degrees();
+        float speed = (current_encoder_degrees - *last_encoder_degrees) / dt;
+        last_encoder_degrees = current_encoder_degrees;
+        if (speed <= MIN_STALL_DPS) {
+            if (calibration_hold_time) {
+                if (get_tick_seconds() - *calibration_hold_time >
+                        MIN_CALIBRATION_STALL_DELTA) {
+
+                    // temporary value
+                    encoder.count = calibration_encoder_offset;
+                    switch_states(state::ACTIVE);
+                }
+            } else {
+                calibration_hold_time = get_tick_seconds();
+            }
+        } else {
+            calibration_hold_time = std::nullopt;
+        }
+    }
 }
 
 } // namespace sdk
